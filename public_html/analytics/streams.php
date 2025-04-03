0c953247474b0b983821df6cd8483d675e6ff9e9
aznarez138165 <aznarez.138165@e.unavarra.es>
2025-04-04 00:16:59 +0200

    [#67][#63] - Modificado el status code a 500 y el texto de respuesta cuando hay internal server error

--- public_html/analytics/streams.php
+++ public_html/analytics/streams.php
@@ -7,6 +7,12 @@ require '../verificarToken.php';
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
+set_error_handler(function ($errno, $errstr, $errfile, $errline) {
+    http_response_code(500);
+    echo json_encode(["error" => "Internal server error."]);
+    exit;
+});
+
 $headers = apache_request_headers();
 if (!isset($headers['Authorization']) || !preg_match('/Bearer\s(\S+)/',
         $headers['Authorization'], $matches)) {
