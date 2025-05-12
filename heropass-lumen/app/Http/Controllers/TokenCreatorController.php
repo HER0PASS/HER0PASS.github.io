069138a8391091a79b6860cbea62d5bfbac1c847
munoz138255 <munoz.138255@e.unavarra.es>
2025-05-12 22:42:26 +0200

    [#107][#108] - Creación del Collector y Validator para el endpoint de Token.php

---
+++ heropass-lumen/app/Http/Controllers/TokenCreatorController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Exceptions\EmptyApiKeyException;
+use App\Exceptions\EmptyEmailException;
+use App\Exceptions\InvalidApiKeyException;
+use App\Exceptions\InvalidEmailAddressException;
+use App\Services\GetApiKeyService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class TokenCreatorController
+{
+    private TokenCreatorValidator $validator;
+    private GetApiKeyService $service;
+
+    public function __construct(
+        TokenCreatorValidator $validator,
+        GetApiKeyService $service,
+    ) {
+        $this->validator = $validator;
+        $this->service = $service;
+    }
+    public function createToken(Request $request): JsonResponse
+    {
+        try {
+            $email = $this->validator->validateEmail($request->input('email'));
+            $apiKey = $this->validator->validateApiKey($request->input('api_key'));
+
+            return new JsonResponse(['email' => $email, 'api_key' => $apiKey], 200);
+        } catch (EmptyEmailException | InvalidEmailAddressException | EmptyApiKeyException | InvalidApiKeyException $e) {
+            return new JsonResponse([
+                'error' =>  $e->getMessage(),
+            ], 400);
+        }
+    }
+}
