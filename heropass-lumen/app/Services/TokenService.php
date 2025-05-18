8638499a01f09d65d3cfe0af2f5017e0101c3d87
molina147893 <cuestionado.agr@gmail.com>
2025-05-18 02:08:36 +0200

    [#124] - Arreglar actualización del token

--- heropass-lumen/app/Services/TokenService.php
+++ heropass-lumen/app/Services/TokenService.php
@@ -27,7 +27,7 @@ class TokenService
         $token = $data['token'] ?? null;
         $expires_at = $data['expires_at'] ?? null;
 
-        if ($token === null | $expires_at < time()) {
+        if ($token === null || strtotime($expires_at) < time()) {
             $response = $this->tokenManager->generateToken();
 
             $data = json_decode($response->getContent(), true);
