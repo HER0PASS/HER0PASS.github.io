f0bb35eafee9f3e325f74f19959c2a6a8c50d88a
aznarez138165 <aznarez.138165@e.unavarra.es>
2025-05-23 20:12:29 +0200

    [#134][#139] - Test unitarios del EmailValidator hechos

---
+++ heropass-lumen/tests/Register/Unit/EmailValidatorTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Register\Unit;
+
+use App\Exceptions\EmptyEmailException;
+use App\Exceptions\InvalidEmailAddressException;
+use App\Http\Controllers\Register\EmailValidator;
+use PHPUnit\Framework\TestCase;
+
+class EmailValidatorTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function testValidateGivenNoEmailThrowsException(): void
+    {
+        $validator = new EmailValidator();
+
+        $this->expectException(EmptyEmailException::class);
+        $this->expectExceptionMessage('The email is mandatory');
+
+        $validator->validate(null);
+    }
+
+    /**
+     * @test
+     */
+    public function testRequestIsInvalidateIfEmailAddressGivenIsNotValid(): void
+    {
+        $validator = new EmailValidator();
+
+        $this->expectException(InvalidEmailAddressException::class);
+        $this->expectExceptionMessage('The email must be a valid email address');
+
+        $validator->validate('testNotValid');
+    }
+
+    /**
+     * @test
+     */
+    public function testGivenNotSanitazedMailReturnsSanitazedEmail(): void
+    {
+        $validator = new EmailValidator();
+        $response = $validator->validate('(notSanitazed@mail.com)');
+        $this->assertEquals('notSanitazed@mail.com', $response);
+    }
+}
