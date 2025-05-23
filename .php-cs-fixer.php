58dcf11584c8120ad2eea8edf3fb465ac08095bc
aznarez138165 <aznarez.138165@e.unavarra.es>
2025-05-24 00:15:01 +0200

    [#141] - Corregido error de salto de linea con cs fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -9,5 +9,6 @@ return (new PhpCsFixer\Config())
         '@PSR12' => true, // Cumple PSR-12
         'array_syntax' => ['syntax' => 'short'], // Usa [] en lugar de array()
         'ordered_imports' => ['sort_algorithm' => 'alpha'], // Ordena los imports alfabéticamente
+        'line_ending' => true,
     ])
     ->setFinder($finder);
