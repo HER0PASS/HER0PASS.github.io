98968a35641361be4c96dc2c93b8db81a1e2387b
aznarez138165 <aznarez.138165@e.unavarra.es>
2025-05-23 16:11:02 +0200

    [#94] - Añadidos fixer y codesnifer en directorio heropass-lumen

---
+++ heropass-lumen/.php-cs-fixer.php
@@ -0,0 +1,13 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__) // Aplica en todo el proyecto
+    ->exclude('bootstrap/cache'); // Excluye esta carpeta
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR12' => true, // Cumple PSR-12
+        'array_syntax' => ['syntax' => 'short'], // Usa [] en lugar de array()
+        'ordered_imports' => ['sort_algorithm' => 'alpha'], // Ordena los imports alfabéticamente
+    ])
+    ->setFinder($finder);
