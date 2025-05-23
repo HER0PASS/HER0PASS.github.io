96457a771fdb44a55ead01599aa4a2c36c96b5fd
aznarez138165 <aznarez.138165@e.unavarra.es>
2025-05-23 20:23:15 +0200

    [#134][#139] - Test unitarios del RegisterService hechos

--- heropass-lumen/tests/Fakes/FakeDataBaseRepository.php
+++ heropass-lumen/tests/Fakes/FakeDataBaseRepository.php
@@ -3,11 +3,13 @@
 namespace Tests\Fakes;
 
 use App\Interfaces\DataBaseRepositoryInterface;
+use App\Models\APISessions;
+use App\Models\APIUser;
 use App\Models\TwitchUser;
 
 class FakeDataBaseRepository implements DataBaseRepositoryInterface
 {
-    private array $fakeUsers = [
+    private array $fakeTwitchUsers = [
         [
             'id' => '12345',
             'login' => 'ninja',
@@ -36,7 +38,7 @@ class FakeDataBaseRepository implements DataBaseRepositoryInterface
 
     public function getTwitchUserById(string $id): ?TwitchUser
     {
-        foreach ($this->fakeUsers as $data) {
+        foreach ($this->fakeTwitchUsers as $data) {
             if ($data['id'] === $id) {
                 return TwitchUser::fromArray($data);
             }
@@ -47,5 +49,68 @@ class FakeDataBaseRepository implements DataBaseRepositoryInterface
     public function saveTwitchUser(TwitchUser $user): void
     {
         // TODO: Implement saveUser() method.
+    }
+
+    private array $fakeAPIUsers = [
+        [
+            'id' => '1',
+            'email' => 'user1@example.com',
+            'api_key' => '6288f213b19339919569e8b43f1ad852'
+        ],
+        [
+            'id' => '2',
+            'email' => 'user2@example.com',
+            'api_key' => 'bab8ea158c16e2741c1b7ec1ec14febc'
+        ]
+    ];
+
+    public function getAPIUserByEmail($email): ?APIUser
+    {
+        foreach ($this->fakeAPIUsers as $userData) {
+            if ($userData['email'] === $email) {
+                return APIUser::fromArray($userData);
+            }
+        }
+        return null;
+    }
+
+    public function checkAPIUserExistence(APIUser $apiUser): ?APIUser
+    {
+        // TODO: Implement checkAPIUserExistence() method.
+    }
+
+    public function updateAPIUserAPIKey(APIUser $apiUser): void
+    {
+        foreach ($this->fakeAPIUsers as &$userData) {
+            if ($userData['email'] === $apiUser->getEmail()) {
+                $userData['api_key'] = $apiUser->getApiKey();
+                return;
+            }
+        }
+    }
+
+    public function registerAPIUser(APIUser $apiUser): void
+    {
+        // TODO: Implement registerAPIUser() method.
+    }
+
+    public function getSessionByToken($token): ?APISessions
+    {
+        // TODO: Implement getSessionByToken() method.
+    }
+
+    public function getSessionByUserId($user_id): ?APISessions
+    {
+        // TODO: Implement getSessionByUserId() method.
+    }
+
+    public function registerSession(APISessions $apiSession): void
+    {
+        // TODO: Implement registerSession() method.
+    }
+
+    public function updateSession(APISessions $apiSession): void
+    {
+        // TODO: Implement updateSession() method.
     }
 }
