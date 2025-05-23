e910ea9ac0b12ae627cf5a0e84a8abe254d2fd17
aznarez138165 <aznarez.138165@e.unavarra.es>
2025-05-23 20:56:16 +0200

    [#134][#139] - Test E2E del rendpoint register hecho

---
+++ heropass-lumen/tests/Register/EndToEnd/RegisterEndpointTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Register\EndToEnd;
+
+use App\Interfaces\DataBaseRepositoryInterface;
+use Laravel\Lumen\Testing\TestCase;
+use Tests\Fakes\FakeDataBaseRepository;
+
+class RegisterEndpointTest extends TestCase
+{
+    public function createApplication()
+    {
+        $app = require __DIR__ . '/../../../bootstrap/app.php';
+
+        $app->bind(DataBaseRepositoryInterface::class, FakeDataBaseRepository::class);
+
+        return $app;
+    }
+
+    /**
+     * @test
+     */
+    public function givenValidRequestReturnsAPIKey(): void
+    {
+        $this->post('/register', ['email' => 'e2e@example.com']);
+
+        $this->seeStatusCode(200);
+        $this->seeJsonStructure(['api_key']);
+    }
+}
