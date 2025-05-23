e066de6cf1c4ba9142f00a6420aa4595b4711f40
aznarez138165 <aznarez.138165@e.unavarra.es>
2025-05-23 19:19:31 +0200

    [#134][#137] - Service del register implementado

--- heropass-lumen/app/Services/RegisterService.php
+++ heropass-lumen/app/Services/RegisterService.php
@@ -2,25 +2,29 @@
 
 namespace App\Services;
 
-use App\Repository\DataBaseRepository;
-use Illuminate\Http\JsonResponse;
+use App\Interfaces\DataBaseRepositoryInterface;
+use App\Models\APIUser;
 
 class RegisterService
 {
-    public function __construct(
-        private DataBaseRepository $dataBaseRepository,
-    ) {
-        $this->dataBaseRepository = new DataBaseRepository();
-    }
-    public function registerUser($email): JsonResponse
+    private DataBaseRepositoryInterface $dataBaseRepository;
+
+    public function __construct(DataBaseRepositoryInterface $dataBaseRepository)
     {
-        $api_key = bin2hex(random_bytes(16));
-        $user = $this->dataBaseRepository->getAPIUserByEmail($email);
-        if ($user) {
-            $this->dataBaseRepository->updateAPIUserAPIKey($email, $api_key);
+        $this->dataBaseRepository = $dataBaseRepository;
+    }
+    public function registerUser(APIUser $user): APIUser
+    {
+        $existing = $this->dataBaseRepository->getAPIUserByEmail($user->getEmail());
+
+        $user->generateApiKey();
+
+        if ($existing) {
+            $this->dataBaseRepository->updateAPIUserAPIKey($user);
         } else {
-            $this->dataBaseRepository->registerAPIUser($email, $api_key);
+            $this->dataBaseRepository->registerAPIUser($user);
         }
-        return new JsonResponse(['api_key' => $api_key], 200);
+
+        return $user;
     }
 }
