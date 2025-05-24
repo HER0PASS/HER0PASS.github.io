077028467b3de75c78b5bf8a4f569ff60a9592ca
aznarez138165 <aznarez.138165@e.unavarra.es>
2025-05-24 18:35:25 +0200

    [#160][#161] - Tests de unitarios del autenticador de token hechos

---
+++ tests/Http/Middleware/AuthenticateUnitaryTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Http\Middleware;
+
+use App\Http\Middleware\Authenticate;
+use App\Http\Middleware\TokenManager;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+
+class AuthenticateUnitaryTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->tokenManager = Mockery::mock(TokenManager::class);
+        $this->authenticate = new Authenticate($this->tokenManager);
+    }
+
+    /**
+     * @test
+     */
+    public function givenNoAuthorizationHeaderReturns401()
+    {
+        $request = Request::create('/some-protected-endpoint', 'GET');
+
+        $response = $this->authenticate->handle($request, fn () => new JsonResponse(['success' => true]));
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(401, $response->getStatusCode());
+        $this->assertEquals('Unauthorized. Token is invalid or expired.', $response->getData()->error);
+    }
+
+    /**
+     * @test
+     */
+    public function givenInvalidTokenReturns401()
+    {
+        $this->tokenManager
+            ->shouldReceive('tokenIsActive')
+            ->once()
+            ->andReturn(false);
+
+        $request = Request::create('/some-protected-endpoint', 'GET');
+        $request->headers->set('Authorization', 'Bearer fake_token');
+
+        $response = $this->authenticate->handle($request, fn () => new JsonResponse(['success' => true]));
+
+        $this->assertEquals(401, $response->getStatusCode());
+        $this->assertEquals('Unauthorized. Token is invalid or expired.', $response->getData()->error);
+    }
+
+    /**
+     * @test
+     */
+    public function givenValidTokenGoNextMiddleware()
+    {
+        $this->tokenManager
+            ->shouldReceive('tokenIsActive')
+            ->once()
+            ->andReturn(true);
+
+
+        $request = Request::create('/some-protected-endpoint', 'GET');
+        $request->headers->set('Authorization', 'Bearer valid_token');
+
+        $response = $this->authenticate->handle($request, fn () => new JsonResponse(['ok' => true]));
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertTrue($response->getData()->ok);
+    }
+}
