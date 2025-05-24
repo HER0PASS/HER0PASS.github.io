ec40ae80cfb86f6399907e8df743908530052c97
valles146260 <byvallees17@gmail.com>
2025-05-24 12:34:20 +0200

    [#148][#150] - Implementar twitchAPIRepository

--- app/Repository/TwitchAPIRepository.php
+++ app/Repository/TwitchAPIRepository.php
@@ -47,4 +47,44 @@ class TwitchAPIRepository implements TwitchApiRepositoryInterface
 
         return TwitchUser::fromArray($data["data"][0]);
     }
+
+    public function getStreams(): ?array
+    {
+        $client_id = $this->credentials['client_id'];
+        $access_token = $this->credentials['access_token'];
+
+        $api_url = "https://api.twitch.tv/helix/streams?first=10";
+
+        $headers = [
+            "Client-ID: $client_id",
+            "Authorization: Bearer $access_token"
+        ];
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $api_url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($ch);
+        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        if ($http_code !== 200 || !$response) {
+            return null;
+        }
+
+        $data = json_decode($response, true);
+        if (!isset($data["data"])) {
+            return null;
+        }
+
+        $streams_filtrados = array_map(function ($stream) {
+            return [
+                "title" => $stream["title"],
+                "user_name" => $stream["user_name"],
+            ];
+        }, $data["data"]);
+
+        return $streams_filtrados;
+    }
 }
